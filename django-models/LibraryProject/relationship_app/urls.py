4d08f661998126888f28179e9444ec028fff7a37
bruce wyllis <brucewyllis01@gmail.com>
2025-09-07 18:59:26 +0300

    yeah

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView  # import the built-in auth views
 from . import views
 
 urlpatterns = [
+    # Book and library views
     path('books/', views.list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+
+    # Authentication views
+    path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
+    path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
+    path('register/', views.register_view, name='register'), 
 ]
