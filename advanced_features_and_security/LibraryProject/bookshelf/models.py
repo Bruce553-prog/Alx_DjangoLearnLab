827c5dac243bc95711f24c6230670bb4268a6ef9
bruce wyllis <brucewyllis01@gmail.com>
2025-09-14 20:09:40 +0300

    updating models.py and views.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -2,17 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-# Existing Book model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
 
 
-# Custom user manager
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
@@ -33,7 +39,6 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(username, email, password, **extra_fields)
 
 
-# Custom user model
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
