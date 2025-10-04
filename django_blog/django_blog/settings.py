a280b90238e5f9b5b4f0da3a495d872ced73e539
bruce wyllis <brucewyllis01@gmail.com>
2025-10-04 16:16:48 +0300

    configuring PostgreSQL database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,13 +75,18 @@ STATICFILES_DIRS = [
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'postgres',
+        'PASSWORD': 'your_password_here',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
