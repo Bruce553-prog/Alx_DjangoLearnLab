6d9f824864ca5f7f3885955096dce939be68c900
bruce wyllis <brucewyllis01@gmail.com>
2025-10-04 15:59:44 +0300

    updating the models

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+# Login redirect and logout redirect
+LOGIN_REDIRECT_URL = "blog:profile"   
+LOGOUT_REDIRECT_URL = "blog:login"
+
