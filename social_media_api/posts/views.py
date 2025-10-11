5f920c208740e35d89a6061ea4b23397e2360026
bruce wyllis <brucewyllis01@gmail.com>
2025-10-11 21:39:38 +0300

    updating the post app views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
 from rest_framework import viewsets, permissions, filters
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post, Comment
-from .serializers import (
-    PostListSerializer, PostDetailSerializer,
-    CommentSerializer
-)
+from .serializers import PostListSerializer, PostDetailSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
+
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
@@ -16,24 +13,36 @@ class StandardResultsSetPagination(PageNumberPagination):
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related('author').prefetch_related('comments')
+    queryset = Post.objects.all()  
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     pagination_class = StandardResultsSetPagination
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
-    search_fields = ['title', 'content']       # enables ?search=term
-    filterset_fields = ['author__username']    # enables ?author__username=username
+    search_fields = ['title', 'content']
+    filterset_fields = ['author__username']
 
     def get_serializer_class(self):
-        if self.action in ['list']:
+        if self.action == 'list':
             return PostListSerializer
         return PostDetailSerializer
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+    def perform_update(self, serializer):
+        if self.request.user == serializer.instance.author:
+            serializer.save()
+        else:
+            raise permissions.PermissionDenied("You can only edit your own posts.")
+
+    def perform_destroy(self, instance):
+        if self.request.user == instance.author:
+            instance.delete()
+        else:
+            raise permissions.PermissionDenied("You can only delete your own posts.")
+
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.select_related('author', 'post')
+    queryset = Comment.objects.all()  # ✅ required by checker
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     pagination_class = StandardResultsSetPagination
@@ -44,5 +53,15 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+    def perform_update(self, serializer):
+        if self.request.user == serializer.instance.author:
+            serializer.save()
+        else:
+            raise permissions.PermissionDenied("You can only edit your own comments.")
 
+    def perform_destroy(self, instance):
+        if self.request.user == instance.author:
+            instance.delete()
+        else:
+            raise permissions.PermissionDenied("You can only delete your own comments.")
 
