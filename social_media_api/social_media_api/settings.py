a0b702375a1dd0ae3a3968e52fe509b1caef8c58
bruce wyllis <brucewyllis01@gmail.com>
2025-10-12 10:28:06 +0300

    configuring my database to postgre

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -97,11 +97,14 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_media_db',     
+        'USER': 'postgres',            
+        'PASSWORD': 'yourpassword',   
+        'HOST': 'localhost',           
+        'PORT': '5432',                
     }
 }
 
